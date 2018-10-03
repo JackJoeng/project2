2a781ee795c00631e6bb16d3cbe6926be6001a97
eric11598 <eric.arellano.0803@gmail.com>
2018-10-02 21:42:03 -0400

    lol

--- models/example.js
+++ models/example.js
@@ -4,8 +4,8 @@ module.exports = function(sequelize, DataTypes) {
     email: DataTypes.TEXT,
     name: DataTypes.STRING,
     facebookID: DataTypes.BIGINT(11),
-    friendsList: Sequelize.STRING(1234),
-    matchList: Sequelize.STRING(1234)
+    friendsList: DataTypes.STRING(1234),
+    matchList: DataTypes.STRING(1234)
   });
 
   Example.sync();
